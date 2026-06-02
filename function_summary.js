08dac015c707dafc303435b445df6cb75f91ba37
bayjidalom <bayjidalom1005@gmail.com>
2026-06-02 21:42:44 +0600

    function syntax

---
+++ function_summary.js
@@ -0,0 +1,14 @@
+console.log("===============| function syntax |===============")
+
+function fanOff() {
+    console.log("Click the switch to off the fan.")
+}
+fanOff()
+
+
+function doSum() {
+    console.log("Take note book and pen.")
+    console.log("Do Sum.")
+}
+doSum()
+
